3c7b8377c09ce858296d12207a0ddc462537fbd6
flymperopoulos <filippos.lymperopoulos@students.olin.edu>
2015-02-17 16:40:52 -0500

    first commit to heroku

---
+++ oauth.js
@@ -0,0 +1,9 @@
+var ids = {
+	facebook: {
+	 clientID: '639866922809787',
+	 clientSecret: 'ff4bb71e9ccc903bc1fb3fbab9e5838f',
+	 callbackURL: 'http://127.0.0.1:3000/auth/facebook/callback'
+	}
+}
+
+module.exports = ids;
